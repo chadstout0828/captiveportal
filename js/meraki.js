392d6afc861ea69ecbd2a7c08976ff565a85a23e
chadstout0828 <chadstout0828@gmail.com>
2020-03-02 17:16:18 -0500

    Update meraki.js

--- js/meraki.js
+++ js/meraki.js
@@ -8,11 +8,6 @@ var client_mac = GetURLParameter("client_mac");
 // Print Meraki provided paramaters for Debugging State
 console.log("user_continue_url: "+user_continue_url);
 console.log("client_ip: "+client_ip);
-document.getElementById("baseGrantURL").innerHTML = base_grant_url;
-document.getElementById("userContinueURL").innerHTML = user_continue_url;
-document.getElementById("clientIP").innerHTML = client_ip;
-document.getElementById("clientMAC").innerHTML = client_mac;
-document.getElementById("nodeMAC").innerHTML = node_mac;
 
 // Form Submit handler. 
 document.getElementById('loginForm').onsubmit= function(e){
@@ -40,13 +35,6 @@ function authUser(){
 
 // Button handler function to store the form data and login. 
 function login(){
-    // send the data somewhere like a database
-    var data = {};
-    data.name = document.getElementById("name").value;
-    data.email = document.getElementById("email").value;
-    alert("Hello "+data.name +"\n"+"Thanks for providing your email: "+data.email);
-    console.log("Storing data to db...", data);
-
     // Complete Login
     authUser();
 }
